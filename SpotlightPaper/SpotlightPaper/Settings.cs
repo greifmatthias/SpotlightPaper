a473e96b15f7d5211ec4458d9cb6c24ad1288b41
greif.matthias <greif.matthias@outlook.com>
2018-08-13 19:40:30 +0200

    Added Settings

---
+++ SpotlightPaper/SpotlightPaper/Settings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotlightPaper
+{
+    public class Settings
+    {
+        public Boolean autostart { get; set; }
+        public Boolean changepaper { get; set; }
+
+        public void loadSettings()
+        {
+            // Try to read config
+            if (File.Exists("Config"))
+            {
+                TextReader tr = new StreamReader("Config");
+                this.changepaper = Convert.ToBoolean(tr.ReadLine());
+                this.autostart = Convert.ToBoolean(tr.ReadLine());
+                tr.Close();
+            }
+            else
+            {
+                changepaper = false;
+                autostart = false;
+            }
+        }
+
+        public void saveSettings()
+        {
+            // Save
+            TextWriter tw = new StreamWriter("Config");
+            tw.WriteLine(this.changepaper.ToString());
+            tw.WriteLine(this.autostart.ToString());
+            tw.Close();
+        }
+    }
+}
