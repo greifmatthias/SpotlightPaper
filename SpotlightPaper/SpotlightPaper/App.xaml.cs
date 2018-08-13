652458d817db94940c8c130216fb37b2dd8892f7
greif.matthias <greif.matthias@outlook.com>
2018-08-13 09:17:38 +0200

    Background service
    Tray

--- SpotlightPaper/SpotlightPaper/App.xaml.cs
+++ SpotlightPaper/SpotlightPaper/App.xaml.cs
@@ -2,16 +2,70 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 
 namespace SpotlightPaper
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
+        private MainWindow window;
+        private NotifyIcon trayicon;
+
+        public App()
+        {
+            Boolean running = false;
+
+            // Try to read config
+            if (File.Exists("Config"))
+            {
+                TextReader tr = new StreamReader("Config");
+                running = Convert.ToBoolean(tr.ReadLine());
+                tr.Close();
+            }
+
+            // Setup tray icon
+            trayicon = new NotifyIcon();
+            trayicon.Visible = true;
+
+            runningChanged(running);
+
+            trayicon.DoubleClick += Trayicon_DoubleClick;
+
+            // Init control window
+            window = new MainWindow(running, this);
+        }
+
+        private void Trayicon_DoubleClick(object sender, EventArgs e)
+        {
+            window.Show();
+        }
+
+        public void runningChanged(Boolean isrunning)
+        {
+            // Save
+            TextWriter tw = new StreamWriter("Config");
+            tw.WriteLine(isrunning.ToString());
+            tw.Close();
+
+            // Update tray icon
+            if (isrunning)
+            {
+                trayicon.Icon = new Icon("tray-on.ico");
+                trayicon.Text = "SpotlightPaper is doing great.";
+            }
+            else
+            {
+                trayicon.Icon = new Icon("tray.ico");
+                trayicon.Text = "SpotlightPaper is not doing much.";
+            }
+        }
     }
 }
--- SpotlightPaper/SpotlightPaper/MainWindow.xaml.cs
+++ SpotlightPaper/SpotlightPaper/MainWindow.xaml.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,28 +19,72 @@ namespace SpotlightPaper
     {
         // Vars
         String image;
+        App parent;
 
-        public MainWindow()
+        Timer timer;
+
+        public MainWindow(Boolean running, App parent)
         {
             InitializeComponent();
+
+            // Check if need to run
+            this.parent = parent;
 
             // Get datafolder
             DirectoryInfo folder = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
                 + "\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets");
 
+            // Get latest image
             image = folder.GetFiles()
              .OrderByDescending(f => f.LastWriteTime)
              .First().FullName;
 
-            imgBackground.Source = new BitmapImage(new Uri(image, UriKind.Absolute));
+            //Setup timer
+            timer = new Timer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = 1000 * 60 * 60; // 1 hour
+            timer.Enabled = true;
+
+            if (running)
+            {
+                timer.Start();
+            }
+
+            // Setup wallpapers
+            setPapers(running);
+
+            // Setup UI
+            chEnable.IsChecked = running;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            setPapers(true);
         }
 
         private void chEnable_Checked(object sender, RoutedEventArgs e)
         {
             if (chEnable.IsChecked == true)
             {
+                setPapers(true);
+                timer.Start();
+            }
+            else
+            {
+                timer.Stop();
+            }
+
+            parent.runningChanged(chEnable.IsChecked.Value);
+        }
+
+        private void setPapers(Boolean wallpaperset)
+        {
+            if (wallpaperset)
+            {
                 Wallpaper.Set(new Uri(image), Wallpaper.Style.Centered);
             }
+
+            imgBackground.Source = new BitmapImage(new Uri(image, UriKind.Absolute));
         }
     }
 }
